1bc331df8bfd7ebbabd13b873fefb338100a1274
navope <navopes@mail.ru>
2024-06-05 23:37:26 +0300

    first configuration

---
+++ BotResponder/src/main/java/test/BotResponder/BotResponderApplication.java
@@ -0,0 +1,13 @@
+package test.BotResponder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotResponderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotResponderApplication.class, args);
+	}
+
+}
